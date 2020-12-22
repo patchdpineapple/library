bb97cb2ce93192f6dc7c2341fc14520e60223e73
patchdpineapple <seanpatrick.penaranda@gmail.com>
2020-12-22 20:36:40 +0800

    finished form layout

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+
+
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
@@ -8,3 +10,21 @@ function Book(title, author, pages, status) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status} yet`;
     }
 }
+
+    
+
+    function openForm() {
+        document.getElementById("formcontainer").style.display = "block";
+    }
+
+    function closeForm() {
+        document.getElementById("formcontainer").style.display = "none";
+        
+    }
+
+    function addBook() {
+        document.getElementById("formcontainer").style.display = "none";
+    }
+
+
+
