99a6be3c3ad43206ad4b00e4c5ca363408c07b8e
patchdpineapple <seanpatrick.penaranda@gmail.com>
2021-03-09 16:05:09 +0800

    improved UI. toggleable book color depending if read or unread.

--- code.js
+++ code.js
@@ -1,147 +1,140 @@
-let myLibrary = [
-    // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
-    // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
-    // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
-    // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
-    // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
-    // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
-    // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
-    // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
-    // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
-
+const myLibrary = [
+  // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
+  // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
+  // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
+  // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
+  // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
+  // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
+  // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
+  // { title: 'My Hero Academia', author: 'Masashi Kishimoto', pages: 400, status: 'read' },
+  // { title: 'One Piece', author: 'Eiichiro Oda', pages: 1000, status: 'not read',},
 ];
 
+const gridwrapper = document.querySelector("#gridwrapper");
 
-const gridwrapper = document.querySelector('#gridwrapper');
-
-
-//prototype
+// prototype
 function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.togglestatus = function () {
-        if (this.status == 'read') {
-
-            this.status = 'not read';
-        }
-        else if (this.status == 'not read') {
-
-            this.status = 'read';
-        }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.togglestatus = function () {
+    if (this.status == "Read") {
+      this.status = "Not read";
+    } else if (this.status == "Not read") {
+      this.status = "Read";
     }
+  };
 }
 
 // functions below
 function addBookArray() {
+  const title = document.getElementById("inputTitle").value;
+  const author = document.getElementById("inputAuthor").value;
+  const pages = Number(document.getElementById("inputPages").value);
+  let status;
 
-    const title = document.getElementById('inputTitle').value;
-    const author = document.getElementById('inputAuthor').value;
-    const pages = Number(document.getElementById('inputPages').value);
-    let status;
+  if (document.getElementById("radioRead").checked) {
+    status = "Read";
+  } else if (document.getElementById("radioNotRead").checked) {
+    status = "Not read";
+  }
 
-    if (document.getElementById('radioRead').checked)
-        status = 'read';
-    else if (document.getElementById('radioNotRead').checked)
-        status = 'not read';
+  const objBook = new Book(title, author, pages, status);
+  myLibrary.push(objBook);
 
+  resetForm();
+  document.getElementById("formcontainer").style.display = "none";
 
-    let objBook = new Book(title, author, pages, status);
-    myLibrary.push(objBook);
+  console.clear();
+  console.table(myLibrary);
 
-    resetForm();
-    document.getElementById("formcontainer").style.display = "none";
-
-
-    console.clear();
-    console.table(myLibrary);
-
-    bookgrid.remove();
-    addBookToLibrary(myLibrary);
-
+  bookgrid.remove(); 
+  addBookToLibrary(myLibrary);
 }
 
 function addBookToLibrary(array) {
+  const bookgrid = document.createElement("div");
+  bookgrid.setAttribute("id", "bookgrid");
+  gridwrapper.append(bookgrid);
 
-    const bookgrid = document.createElement('div');
-    bookgrid.setAttribute('id', 'bookgrid');
-    gridwrapper.append(bookgrid);
+  // console.clear();
+  // console.table(myLibrary);
 
-    console.clear();
-    console.table(myLibrary);
+  for (let i = 0; i < array.length; i++) {
+    const book = document.createElement("div");
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const status = document.createElement("button");
+    const deletebook = document.createElement("button");
 
-    for (i = 0; i < array.length; i++) {
+    book.setAttribute("id", `${i}`);
+    book.classList.add("book");
+    status.classList.add("btn-status");
+    deletebook.classList.add("btn-delete");
 
 
-        const book = document.createElement('div');
-        const title = document.createElement('p');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const status = document.createElement('button');
-        const deletebook = document.createElement('button');
+    title.textContent = array[i].title;
+    author.textContent = array[i].author;
+    pages.textContent = array[i].pages;
+    status.textContent = array[i].status;
+    if(array[i].status === "Read") book.classList.add("read");
+    deletebook.textContent = "Delete";
 
-        book.setAttribute('id', `${i}`);
-        book.classList.add('book');
-        deletebook.classList.add('btnDelete');
+    status.addEventListener("click", (e) => {
+      toggleStatus(e);
+      let bookParent = e.target.closest(".book");
+      bookParent.classList.toggle("read");
+    });
+    deletebook.addEventListener("click", deleteOperation);
 
-        title.textContent = array[i].title;
-        author.textContent = array[i].author;
-        pages.textContent = array[i].pages;
-        status.textContent = array[i].status;
-        deletebook.textContent = 'Delete';
-
-        status.addEventListener('click', toggleStatus);
-        deletebook.addEventListener('click', deleteOperation);
-
-        bookgrid.append(book);
-        book.append(title);
-        book.append(author);
-        book.append(pages);
-        book.append(status);
-        book.append(deletebook);
-    }
+    bookgrid.append(book);
+    book.append(title);
+    book.append(author);
+    book.append(pages);
+    book.append(status);
+    book.append(deletebook);
+  }
 }
 
-function toggleStatus() {
-    let x = this.parentElement.id;
-    console.log('toggled id: ' + x);
+function toggleStatus(e) {
+  const target = e.target;
+  const x = parseInt(e.target.closest(".book").id);
+  console.log(x);
 
-    if (this.textContent == 'read') {
-        this.textContent = 'not read';
-        myLibrary[x].status = 'not read';
-    }
-    else {
-        this.textContent = 'read';
-        myLibrary[x].status = 'read';
-    }
+  if (target.textContent === "Read") {
+    target.textContent = "Not read";
+    myLibrary[x].status = "Not read";
+  } else {
+    target.textContent = "Read";
+    myLibrary[x].status = "Read";
+  }
 
-    console.clear();
-    console.table(myLibrary);
+  console.clear();
+  console.table(myLibrary);
 }
 
 function deleteOperation() {
-    let x = this.parentElement.id;
-    console.log('my parent element id is: ' + x);
+  const x = this.parentElement.id;
+  console.log(`my parent element id is: ${x}`);
 
-    myLibrary.splice(x, 1);
+  myLibrary.splice(x, 1);
 
-    bookgrid.remove();
-    addBookToLibrary(myLibrary);
+  bookgrid.remove();
+  addBookToLibrary(myLibrary);
 }
 
-
-
 function openForm() {
-    document.getElementById("formcontainer").style.display = "block";
+  document.getElementById("formcontainer").style.display = "block";
 }
 
 function closeForm() {
-    document.getElementById("formcontainer").style.display = "none";
+  document.getElementById("formcontainer").style.display = "none";
 }
 
 function resetForm() {
-    document.getElementById("addformid").reset();
+  document.getElementById("addformid").reset();
 }
 
 resetForm();
